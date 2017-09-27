c3f66962a3b858313bafa6af00663567dc983746
User10836361 <31942127+User10836361@users.noreply.github.com>
2017-09-27 21:27:01 +1000

    positioning now working
    Changed to a 3DOF model with a 4th imaginary link of 0 length so that I can move the arm to any position without having to worry about the orientation of the 3rd link. Still need to make a function to know the end position of the end effector, partially done with angle calculations.

--- base.m
+++ base.m
@@ -5,28 +5,22 @@ clc
 goalXYZ= zeros(3,1);
 eStop =zeros(1,8); 
 %%%%%%%%%  Initialise myR1 robot
-L1 = Link('d',0,'a',2,'alpha',3*pi/2,'offset',0,'qlim', [-pi/2,pi/2]);
+L1 = Link('d',0,'a',2,'alpha',-pi/2,'offset',0,'qlim', [-pi/2,pi/2]);
 L2 = Link('d',0,'a',15,'alpha',0,'offset',-pi/1.5,'qlim', [(-1/3*pi),(1/3*pi)]);
 L3 = Link('d',0,'a',16,'alpha',0,'offset',+pi/1.5,'qlim', [-pi/3,(2/3*pi)]);
-L4 = Link('d',0,'a',6.95,'alpha',0,'offset',degtorad(59.7),'qlim', [-pi/2,pi/2]);
+L4 = Link('d',0,'a',0,'alpha',0,'offset',0,'qlim', [-2*pi,2*pi]);
 
 uARM = SerialLink([L1 L2 L3 L4], 'name', 'uARM')
-uARM.base = transl(0.5, 0, -2); %Base position uARM
+uARM.base = transl(0, 0, 0); %Base position uARM
 
 q1 =0; q2 =0; q3 =0; q4 =0; %initialise q states
 qa = [q1,q2,q3,q4];
 uARM.plot(qa);
 hold on;
-goalXYZ = [10,10,0];
+goalXYZ = [5,15,10];
 moveTo(goalXYZ,eStop,qa);
-
-
 uARM.teach
-% qValues = uARM.fkine(qa)
-%guess = uARM.ikcon(transl(goalXYZ),qa,[0,0,0,0,0,1])
-%degguess = radtodeg(guess)
-%End effector is 30.3 degrees clockwise from L4 position so that it is
-%normal to the xy = 0 plane
+
 function iseStop(eStop)
 stopped = any(eStop);
  while stopped ~= 0
@@ -38,25 +32,36 @@ function moveTo(goalXYZ,eStop,qa)
 iseStop(eStop);  %check for safety
 steps = 15;
 %get angles
-
-
 endEffector = uARM.fkine(qa)
 qNow = uARM.ikcon(uARM.fkine(qa)) %current joint angles
-goalPoint = [endEffector(1,1),endEffector(1,2),endEffector(1,3),goalXYZ(1,1); endEffector(2,1),endEffector(2,2),endEffector(2,3),goalXYZ(1,2); endEffector(3,1),endEffector(3,2),endEffector(3,3),goalXYZ(1,3);endEffector(4,1),endEffector(4,2),endEffector(4,3),endEffector(4,4);]
+%required L1 rotation to get end effector pose
+angleGoal = radtodeg(atan((goalXYZ(1,1)/goalXYZ(1,2))))
+angleCurrent =radtodeg( atan((endEffector(1,4)/endEffector(2,4))))
+angleTotal = angleGoal + angleCurrent
+
+reach = (abs(goalXYZ(1,1))+abs(goalXYZ(1,2))) %of arm gives the amount of reach needed which then determines the
+%angle of L3 with regards to the height required.
+if reach > 30
+    disp('Out of range')
+end
+if reach < 10
+    disp('Out of range')
+end
+if goalXYZ(1,3) > 19
+    disp('Out of range')
+end
+if goalXYZ(1,3) < 0
+    disp('Out of range')
+end
+
+%end effector wanted orientation (L3 orientation) 
+arg = rotz(angleTotal)%*rotx(16.3-((reach/33)*(goalXYZ(1,3)/25)*16.3)) %(16.3/((goalXYZ(1,3)/6.5)*(reach/10.5)))
+
+%now goalXYZ offset to account for L4 and end effector
+
+goalPoint = [arg(1,1),arg(1,2),arg(1,3),goalXYZ(1,1); arg(2,1),arg(2,2),arg(2,3),goalXYZ(1,2); arg(3,1),arg(3,2),arg(3,3),goalXYZ(1,3);endEffector(4,1),endEffector(4,2),endEffector(4,3),endEffector(4,4);]
 qValues = uARM.ikcon(goalPoint) %goal joint values
-%get values for each link joint
-%qL1 = %rotation of arm about the base
-qL1 = qValues(1,1)
-%qL2 = %rotation of side arm 
-qL2 = qValues(1,2)
-%qL3 = %rotation of top arm function of other arms
-qL3 = qValues(1,3)%540-((210+radtodeg(qL2))+330) %angle of L2, plus angles of 5 sided shape
-%qL4 = %rotation of end effector with 30.3 degree ofset so that it points normal to ground function of other arms
-qL4 = -(radtodeg(qL2)+qL3)    %qValues(1,4)    %effector at normal to ground, undo rotations of other arms
-%Actual move part
-qGoal = [qL1,qL2,degtorad(qL3),degtorad(qL4)]
-qMatrix = jtraj(qNow,qGoal,steps)
-%tMatrix = ctraj(endEffector,goalPoint,15) %give point by point movement in transforms
+qMatrix = jtraj(qNow,qValues,steps)
 while steps >= 1
             iseStop(eStop);
             uARM.plot(qMatrix((16-steps),:));
